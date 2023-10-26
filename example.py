1e8ebbde6afe173fa337b86c844078150f5d5499
Gaolaboratory <yugao@uic.edu>
2023-10-25 19:45:26 -0500

    added gzip files and support and ipynb

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import gzip
 
-with gzip.open(r"C:\Users\bathy\Desktop\Coding_training\UniProt_Human.fasta", "rt") as fin:
+with gzip.open("UniProt_Human.fasta.gz", "rt") as fin:
     split_list = [protein for protein in fin.read()[1:].split('\n>')]
 
 split_line = split_list[0].split('\n')
