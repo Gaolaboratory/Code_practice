b14c2f0a1e20ead1bef58361f3137c1cf1ca1bc1
Gaolaboratory <yugao@uic.edu>
2023-10-25 19:43:11 -0500

    added gzip files and support

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-with open(r"C:\Users\bathy\Desktop\Coding_training\UniProt_Human.fasta") as fin:
+import gzip
+
+with gzip.open(r"C:\Users\bathy\Desktop\Coding_training\UniProt_Human.fasta", "rt") as fin:
     split_list = [protein for protein in fin.read()[1:].split('\n>')]
 
 split_line = split_list[0].split('\n')
